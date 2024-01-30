f88d96011d73c4df5b7a0b38dd9a5d4038b729d7
Martin Felbaba <martin.felbaba@gmail.com>
2024-01-30 19:27:07 +0100

    JS code introduction

---
+++ index.js
@@ -0,0 +1,10 @@
+// name of file: index.js
+
+var number = 5;
+let name = "Martin";
+number = 12;
+name = "Pluralsight";
+
+// writing to document (DOM)
+document.writeln(name + number);
+//alert("Hello World");
